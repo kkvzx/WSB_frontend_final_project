1f085f295cf8fd3caeebeb3c2d649706bc318a6e
kuczala.sebastian <kuczalasebastian97@gmail.com>
2023-04-22 15:17:09 +0200

    siedzenia

--- src/scripts/summaryView.ts
+++ src/scripts/summaryView.ts
@@ -19,3 +19,31 @@ function createSeat(top: any, left: any, text: string) {
     return seat;
 }
 
+<<<<<<< HEAD
+=======
+// --- funkcja tworząca rzędy i miejsca w samolocie
+function addSeats() {
+    let left = 415;
+    let seatSize = 30;
+    let offsetX = 10;
+    let offsetY = 20;
+    let corridor = 30;
+    let array = ['A','B','C','D','E','F'];
+
+    for(let i = 1; i <= rows; i++) {
+        let top = 35;
+
+        for(let j = 0; j<seatsInRow; j++) {
+            let text = i + array[j];
+            samolot.appendChild(createSeat(top, left, text));
+
+            if(j == 2) top += corridor + seatSize;
+            else top += offsetX + seatSize;
+        }
+
+        left += offsetY + seatSize;
+    }
+}
+
+addSeats();
+>>>>>>> 946c315 (siedzenia)
