c5209f20b078a24346f7297d1379fc05ab2f34b6
kkvzx <konrvd.kvzx@gmail.com>
2023-05-13 00:22:59 +0200

    removed unused variable

--- src/scripts/mainView/constants.ts
+++ src/scripts/mainView/constants.ts
@@ -2,7 +2,6 @@ import { AirportData } from './interfaces/AirportData';
 import { DropdownData } from './interfaces/DropdownData';
 import { FlightModel } from './interfaces/FlightModel';
 
-// export const API_KEY = '751c5fccbbd330b54786c41ed91cfff3';
 export const OPEN_WEATHER_API_KEY = 'a48ebb02d984830ced1a889260d4cbd9';
 export const OPENCAGEDATA_API_KEY = '765eb0efe3f644808f086b836bf0255e'; // Replace with your OpenCage API key
 
